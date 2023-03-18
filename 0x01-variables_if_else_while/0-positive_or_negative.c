0845c7e36e175112c114e853cd2a12ebecc1c54a
MOn <minaraafatemil14@gmail.com>
2023-03-18 14:58:53 -0700

    M

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 #include<stdio.h>
-/* betty style doc for function main goes there */
 /**
- * main -main function
+ * main - main function
  *
- * return: always 0
+ * Return: always 0
  */
 
 int main(void)
