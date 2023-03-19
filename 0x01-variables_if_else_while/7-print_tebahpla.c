aac99435c22a44cf43097d06d7071e0336521c8c
MOn <minaraafatemil14@gmail.com>
2023-03-19 12:34:38 -0700

    M

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z' ; i <= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
