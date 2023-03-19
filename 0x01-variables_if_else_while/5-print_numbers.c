0672f5fcc577b007b61dab497e1c38b386c96f33
MOn <minaraafatemil14@gmail.com>
2023-03-19 12:23:53 -0700

    M

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+		printf("%d", i);
+	putchar('\n');
+	return (0);
+}
