fa54405733ad05a07f96e24f84f27e40636c86a8
MOn <minaraafatemil14@gmail.com>
2023-03-19 12:07:34 -0700

    M

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,11 +9,11 @@
  */
 int main(void)
 {
-	int i;
+	char i;
 
 	for (i = 'a' ; i <= 'z' ; i++)
 		putchar(i);
-	for (i ='A' ; i <= 'z'; i++)
+	for (i = 'A' ; i <= 'Z'; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
