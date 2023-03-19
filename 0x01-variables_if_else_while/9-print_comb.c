282540a8c02798bd993be4450b06f247fdeb03fc
MOn <minaraafatemil14@gmail.com>
2023-03-19 12:53:26 -0700

    M

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
