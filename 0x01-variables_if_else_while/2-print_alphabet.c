e5de5c687f92733e110decbce288b5361dd82353
MOn <minaraafatemil14@gmail.com>
2023-03-19 11:48:40 -0700

    M

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include<stdio.h>
+/**
+ * main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 'a' ; i < 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
