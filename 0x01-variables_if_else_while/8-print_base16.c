b9c0157a595ab86539088338498de5bfe9d0ffd7
MOn <minaraafatemil14@gmail.com>
2023-03-19 12:43:18 -0700

    M

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i;
 
-	for (i = 0 ; i <= 10 ; i++)
+	for (i = 0 ; i < 10 ; i++)
 		putchar(i + '0');
-	for (i = 'a' ; i <= 'z' ; i++)
+	for (i = 'a' ; i <= 'f' ; i++)
 		putchar(i);
 	putchar('\n');
 	return (0);
