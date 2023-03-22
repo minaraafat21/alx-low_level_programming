d959638bb3be3a7a9d049fbf8d3ed8bf7e072d36
MOn <minaraafatemil14@gmail.com>
2023-03-22 14:57:40 -0700

    5

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - checks for absolute value
+ *
+ * @r: parameter
+ *
+ * Return: returns same number if positive else negative
+ *
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	return (-r);
+}
