ef754559204b16f64e3e2cc8a56758cdcdcf336c
MOn <minaraafatemil14@gmail.com>
2023-04-13 15:38:05 -0700

    MON

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: number of bytes to allocate
+ * Return: pointer on success
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
